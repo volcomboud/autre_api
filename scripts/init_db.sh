e1c7abe9a12b81d39ea2ea4cb5fe7dd27f5bfe1e
volcomboud <83305133+volcomboud@users.noreply.github.com>
2023-02-21 07:49:55 -0500

    Update init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -17,8 +17,7 @@ if ! [ -x "$(command -V sqlx)" ];
   then
     echo >&2 "Error sqlx is not installed"
     echo >&2 "Use : "
-    echo >&2 "cargo install --version='~0.6' sqlx-cli \
-    --no-default-features --features rustls,postgres"
+    echo >&2 "cargo install --version='~0.6' sqlx-cli --no-default-features --features rustls,postgres"
     echo >&2 "to install it"
     exit 1
 fi
